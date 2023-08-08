4724440320ff32612cedb11886aa2bbfd1e77500
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-08 19:10:05 +0200

    Processing Mangas

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use std::path::{Path, PathBuf};
+use std::io::Write;
+use std::{
+    fs::{read_to_string, File},
+    path::{Path, PathBuf},
+    process::Command,
+};
 
+use clap::{Parser, Subcommand};
+use regex::Regex;
 use reqwest::blocking::multipart::Form;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 struct Manga {
     name: String,
     mal_url: String,
-    thumbnail: PathBuf,
+    cover: PathBuf,
 }
 
 impl Manga {
@@ -15,9 +22,30 @@ impl Manga {
         Form::new()
             .text("name", self.name.clone())
             .text("malUrl", self.mal_url.clone())
-            .file("thumbnail", self.thumbnail.clone())
+            .file("cover", self.cover.clone())
             .unwrap()
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MangaSpec {
+    name: String,
+    #[serde(rename = "malUrl")]
+    mal_url: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct ChapterSpec {
+    index: usize,
+    path: PathBuf,
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProcessedChapter {
+    index: usize,
+    name: String,
+    pages: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -59,39 +87,231 @@ fn create_manga(manga: &Manga) -> Option<MangaResponse> {
     }
 }
 
-fn main() {
+fn test_create_chapter() {
+    let endpoint = "http://127.0.0.1:8090";
+
     let manga = Manga {
         name: "Oshi no Ko".to_string(),
         mal_url: "https://myanimelist.net/manga/126146/Oshi_no_Ko".to_string(),
-        thumbnail: PathBuf::from("/home/nanoteck137/wallpaper.png"),
+        cover: PathBuf::from("/home/nanoteck137/wallpaper.png"),
     };
 
     // let res = create_manga(&manga);
     // println!("Res: {:#?}", res);
     // return;
-    
+
     let client = reqwest::blocking::Client::new();
 
     let form = Form::new()
-        .text("num", "0")
-        .text("name", "Wot")
+        .text("num", 0.to_string())
+        .text("name", "Ch. 102")
         .text("manga", "18f6sxhqycha8z9")
-        .file("pages", "/home/nanoteck137/p/page-000.png").unwrap()
-        .file("pages", "/home/nanoteck137/p/page-001.png").unwrap()
-        .file("pages", "/home/nanoteck137/p/page-002.png").unwrap()
-        .file("pages", "/home/nanoteck137/p/page-003.png").unwrap()
-        .file("pages", "/home/nanoteck137/p/page-004.png").unwrap()
-        .file("pages", "/home/nanoteck137/p/page-005.png").unwrap()
-        ;
+        .file("pages", "/home/nanoteck137/p/page-000.png")
+        .unwrap()
+        .file("pages", "/home/nanoteck137/p/page-001.png")
+        .unwrap()
+        .file("pages", "/home/nanoteck137/p/page-002.png")
+        .unwrap()
+        .file("pages", "/home/nanoteck137/p/page-003.png")
+        .unwrap()
+        .file("pages", "/home/nanoteck137/p/page-004.png")
+        .unwrap()
+        .file("pages", "/home/nanoteck137/p/page-005.png")
+        .unwrap();
 
     let collection = "chapters";
     let res = client
         .post(format!(
-            "http://127.0.0.1:8090/api/collections/{}/records",
-            collection
+            "{}/api/collections/{}/records",
+            endpoint, collection
         ))
         .multipart(form)
         .send()
         .unwrap();
     println!("Res: {:#?}", res.json::<serde_json::Value>());
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    ProcessManga { path: PathBuf },
+}
+
+fn read_manga_spec<P>(manga_spec: P) -> Option<MangaSpec>
+where
+    P: AsRef<Path>,
+{
+    let s = read_to_string(manga_spec).ok()?;
+
+    serde_json::from_str::<MangaSpec>(&s).ok()
+}
+
+fn process_chapter<P>(chapter: ChapterSpec, path: P) -> (ChapterSpec, Vec<String>)
+where
+    P: AsRef<Path>,
+{
+    // pdfimages -png \[0001\]_Chapter_1.pdf ./page
+    let path = path.as_ref();
+    let mut out_path = path.to_path_buf();
+    out_path.push("page");
+
+    let status = Command::new("pdfimages")
+        .arg("-png")
+        .arg(&chapter.path)
+        .arg(out_path)
+        .status()
+        .unwrap();
+    println!("{} - Status: {}", chapter.index, status.code().unwrap());
+
+    let paths = std::fs::read_dir(path)
+        .unwrap()
+        .map(|res| res.unwrap().path())
+        .collect::<Vec<_>>();
+    // println!("Paths: {:?}", paths);
+
+    let regex = Regex::new(r"page-(\d+).png").unwrap();
+
+    let mut pages = Vec::new();
+    for path in paths {
+        let filename = path.file_name().unwrap().to_string_lossy();
+        let captures = regex.captures(&filename).unwrap();
+        let page_num = (&captures[1]).parse::<usize>().unwrap();
+        pages.push((page_num, path));
+    }
+
+    pages.sort_by(|l, r| l.0.cmp(&r.0));
+
+    let pages = pages
+        .iter()
+        .map(|p| {
+            let file = p.1.file_name().unwrap().to_string_lossy();
+            return String::from(file);
+        })
+        .collect::<Vec<_>>();
+
+    return (chapter, pages);
+}
+
+fn process_manga(path: PathBuf) {
+    if !path.is_dir() {
+        panic!("Path is not a directory");
+    }
+
+    let mut manga_spec = path.clone();
+    manga_spec.push("manga.json");
+
+    let mut chapters_dir = path.clone();
+    chapters_dir.push("chapters");
+
+    let mut processed_json = path.clone();
+    processed_json.push("processed.json");
+
+    let mut processed_dir = path.clone();
+    processed_dir.push("processed");
+
+    if !processed_dir.is_dir() {
+        std::fs::create_dir(&processed_dir).unwrap();
+    }
+
+    if !chapters_dir.is_dir() {
+        panic!("No chapters directory inside path");
+    }
+
+    let manga = read_manga_spec(&manga_spec).unwrap();
+    println!("Manga Spec: {:#?}", manga);
+
+    let paths = std::fs::read_dir(chapters_dir)
+        .unwrap()
+        .map(|res| res.unwrap().path())
+        .collect::<Vec<_>>();
+
+    let regex = Regex::new(r"\[(\d+)\]_\w+_([\d.]+).pdf").unwrap();
+
+    let mut chapters = Vec::new();
+    for path in paths {
+        let file_name = path.file_name().unwrap().to_string_lossy();
+
+        let cap = regex.captures(&file_name).unwrap();
+        let index = &cap[1];
+        let ch_name = &cap[2];
+
+        chapters.push(ChapterSpec {
+            index: index.parse::<usize>().unwrap(),
+            path: path.to_path_buf(),
+            name: ch_name.to_string(),
+        });
+    }
+
+    let mut unprocessed_chapters = Vec::new();
+
+    let mut processed_chapters;
+
+    if processed_json.is_file() {
+        let s = read_to_string(&processed_json).unwrap();
+        processed_chapters = serde_json::from_str::<Vec<ProcessedChapter>>(&s).unwrap();
+
+        for ch in chapters.iter() {
+            let res = processed_chapters.iter().find(|i| i.index == ch.index);
+            if res.is_none() {
+                unprocessed_chapters.push(ch.clone());
+            }
+        }
+    } else {
+        unprocessed_chapters = chapters;
+        processed_chapters = Vec::new();
+    }
+
+    unprocessed_chapters.sort_by(|l, r| l.index.cmp(&r.index));
+    // println!("Chapters: {:#?}", chapters);
+
+    let mut threads = Vec::new();
+    for chapter in unprocessed_chapters.iter() {
+        let mut path = processed_dir.clone();
+        path.push(chapter.index.to_string());
+        if path.is_dir() {
+            std::fs::remove_dir_all(&path).unwrap();
+            println!("'{} - {}' exists inside processed dir", chapter.index, chapter.name);
+        }
+
+        std::fs::create_dir(&path).unwrap();
+
+        let ch = chapter.clone();
+        let p = path.clone();
+        let handle = std::thread::spawn(|| {
+            return process_chapter(ch, p);
+        });
+        threads.push(handle);
+    }
+
+    for handle in threads {
+        let res = handle.join().unwrap();
+
+        processed_chapters.push(ProcessedChapter {
+            index: res.0.index,
+            name: format!("Ch. {}", res.0.name),
+            pages: res.1,
+        });
+    }
+
+    processed_chapters.sort_by(|l, r| l.index.cmp(&r.index));
+
+    let s = serde_json::to_string_pretty(&processed_chapters).unwrap();
+    let mut file = File::create(processed_json).unwrap();
+    writeln!(file, "{}", s).unwrap();
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Args: {:#?}", args);
+
+    match args.command {
+        Commands::ProcessManga { path } => process_manga(path),
+    }
 }
